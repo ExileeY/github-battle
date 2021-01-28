948348b1138722b6fb24b488520dc8807a8ece35
Danila Glazov <danikglazov@gmail.com>
2021-01-28 13:00:02 +0300

    Class Fields

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -1,26 +1,9 @@
 import React from 'react'
 
 export default class Hover extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hovering: false
-    }
-
-    this.onMouseOver = this.onMouseOver.bind(this)
-    this.onMouseOut = this.onMouseOut.bind(this)
-  }
-  onMouseOver() {
-    this.setState({
-      hovering:true
-    })
-  }
-  onMouseOut() {
-    this.setState({
-      hovering:false
-    })
-  }
+  state = { hovering: false }
+  onMouseOver = () => this.setState({ hovering:true })
+  onMouseOut = () => this.setState({ hovering:false })
   render() {
     return (
       <div onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut}>
